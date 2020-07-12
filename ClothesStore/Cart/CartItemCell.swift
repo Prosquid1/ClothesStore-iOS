55c5ca36923d72696950dcff7cc784ac7f5b42ed
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-12 10:30:22 +0100

    WIP Cart Item cell views

---
+++ ClothesStore/Cart/CartItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  CartItemCell.swift
+//  ClothesStore
+//
+//  Created by Oyeleke Okiki on 7/12/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class CartItemCell: UITableViewCell {
+
+    public static let identifier = "CartItemCellID"
+    @IBOutlet weak var genericProductView: GenericProductView!
+    //TODO: Add listeners
+
+}
