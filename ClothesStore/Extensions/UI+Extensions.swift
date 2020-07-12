769f69ccc18bf1cc81dd60673640b6af3dc0e8a0
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-12 10:40:10 +0100

    Extensions and view helpers

---
+++ ClothesStore/Extensions/UI+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  UI+Extensions.swift
+//  ClothesStore
+//
+//  Created by Oyeleke Okiki on 7/12/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+extension CGRect {
+    var minEdge: CGFloat {
+        return min(width, height)
+    }
+}
+
+extension UIRefreshControl {
+    func beginRefreshingManually() {
+        if let scrollView = superview as? UIScrollView {
+            scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentOffset.y - frame.height), animated: true)
+        }
+        beginRefreshing()
+    }
+}
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
