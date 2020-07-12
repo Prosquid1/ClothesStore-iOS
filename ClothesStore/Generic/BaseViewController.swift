2692174a9d665c91fb1a00f8e00a6df3a8cd7bcc
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-12 10:23:34 +0100

    BaseController implementation

---
+++ ClothesStore/Generic/BaseViewController.swift
@@ -0,0 +1,60 @@
+//
+//  BaseViewController.swift
+//
+//  Created by Oyeleke Okiki on 11/7/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+import NVActivityIndicatorView
+import SwiftEntryKit
+
+class BaseViewController: UITableViewController, NVActivityIndicatorViewable {
+
+    var _refreshControl: UIRefreshControl!
+    var refreshStarted: (() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        setup()
+    }
+
+    private func setup() {
+        tableView.separatorInset.left = 0
+        tableView.separatorColor = .clear
+
+        _refreshControl = UIRefreshControl()
+        _refreshControl.tintColor = .lightGray
+        if #available(iOS 10.0, *) {
+            refreshControl = _refreshControl
+        } else {
+            tableView.addSubview(_refreshControl)
+        }
+        // uikit issue-fix: called to change tint color
+        _refreshControl.beginRefreshingManually()
+        _refreshControl.endRefreshing()
+
+        _refreshControl.addTarget(self, action: #selector(refreshTriggered(_:)), for: .valueChanged)
+    }
+
+    @objc private func refreshTriggered(_ sender: Any) {
+        // Fetch Services Data
+        refreshStarted?()
+    }
+
+    func startNVAnimation() {
+        if isAnimating { return }
+        startAnimating(CGSize(width: 50.0, height: 50.0),
+                       type: NVActivityIndicatorType.ballScaleRipple,
+                       color: ColorPalette.primaryDark, fadeInAnimation: nil)
+    }
+
+    func stopNVAnimation() {
+        if !isAnimating { return }
+        stopAnimating()
+    }
+
+}
