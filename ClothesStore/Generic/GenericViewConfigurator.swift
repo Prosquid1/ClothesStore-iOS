0715ea2c9a98d5fbb82502d7e69ceb5ffd960c76
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-13 14:16:03 +0100

    Refactored ProductViewConfigurator to GenericViewConfigurator

--- ClothesStore/Generic/GenericViewConfigurator.swift
+++ ClothesStore/Generic/GenericViewConfigurator.swift
@@ -1,5 +1,5 @@
 //
-//  ProductViewConfigurator.swift
+//  GenericViewConfigurator.swift
 //  ClothesStore
 //
 //  Created by Oyeleke Okiki on 7/12/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProductViewConfigurator {
+class GenericViewConfigurator {
     static func configure(product: Product,
                           genericProductView: GenericProductView) {
 
@@ -22,20 +22,14 @@ class ProductViewConfigurator {
 
         let stockCountHelper = StockCountHelper.getCountType(count: product.stock)
 
-        configureAddToCartButton(stockCountHelper, addToCartButton: genericProductView.addToCartButton)
-        configureStockCountLabel(count, stockCountHelper:stockCountHelper , stockCountLabel: genericProductView.productStockCountLabel)
+        configureStockCountLabel(product.stock, stockCountHelper:stockCountHelper , stockCountLabel: genericProductView.productStockCountLabel)
 
         configureSoldOutText(stockCountHelper == .none , soldOutLabel: genericProductView.productSoldOutText)
         configureProductOldPrice(product.oldPrice,
                                  oldPriceLabel: genericProductView.productOldPriceLabel, spacingConstraint: genericProductView.stockToPriceLabelsXt)
     }
 
-    private static func configureAddToCartButton(_ stockCountHelper: StockCountHelper, addToCartButton: UIButton) {
-        addToCartButton.alpha = stockCountHelper == .none ? 0.34 : 1.00
-        addToCartButton.isEnabled = stockCountHelper != .none
-    }
-
-    private static func configureStockCountLabel(_ count: Int, stockCountHelper: StockCountHelper, _ stockCountLabel: UILabel) {
+    private static func configureStockCountLabel(_ count: Int, stockCountHelper: StockCountHelper, stockCountLabel: UILabel) {
         stockCountLabel.textColor = stockCountHelper.colorValue
         stockCountLabel.text = StockCountHelper.getFormattedCountText(count)
     }
@@ -56,37 +50,5 @@ class ProductViewConfigurator {
             return
         }
         soldOutLabel.rotate(degrees: 315.0)
-    }
-}
-
-enum StockCountHelper {
-    case none
-    case low
-    case high
-
-    var colorValue: UIColor {
-        get {
-            switch self {
-            case .none: return ColorPalette.red
-            case .low: return ColorPalette.green
-            case .high: return ColorPalette.moneyBlue
-            }
-        }
-    }
-
-    var isSoldOut: Bool {
-        if (self == .none) { return false }
-        return true
-    }
-
-    static func getCountType(count: Int) -> StockCountHelper {
-        if (count == 0) { return .none }
-        if (count < 3) { return .low }
-        return .high
-    }
-
-    static func getFormattedCountText(_ count: Int) -> String {
-        if (count == 0) { return "Out of stock" }
-        return "\(count) left"
     }
 }
