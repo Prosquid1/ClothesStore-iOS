f75997d4f87f14734c99952d3ad4af5864a40939
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-13 14:29:39 +0100

    Fixed issue with strikethrough text

--- ClothesStore/Extensions/String+Extensions.swift
+++ ClothesStore/Extensions/String+Extensions.swift
@@ -10,10 +10,12 @@ import Foundation
 import UIKit
 
 extension String {
-    func strikeThrough() -> NSAttributedString  {
-        return NSAttributedString(string: self, attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single])
+    func strikeThrough() -> NSAttributedString{
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: self)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+        return attributeString
     }
-
+    
     func formatPrice() -> String {
         return "£\(self)"
     }
