d17b2ccb21023fdcabcd9f7f7fc9671cc4dbb5d9
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-15 06:28:49 +0100

    WIP MappedCartProductsDelegate implementation on CartDataSourcePresenter

--- ClothesStore/Cart/CartDataSourcePresenter.swift
+++ ClothesStore/Cart/CartDataSourcePresenter.swift
@@ -10,11 +10,35 @@ import Foundation
 
 class CartDataSourcePresenter: DataSourcePresenter<CartItem> {
 
-//    func fetchProducts() -> {
-//
-//    }
+    var mappedCartProductsDelegate: MappedCartProductsDelegate?
 
-     func itemForRow(row: Int) -> Product? {
+    init(dataControllerDelegate: DataControllerDelegate,
+         mappedCartProductsDelegate: MappedCartProductsDelegate) {
+        super.init(dataControllerDelegate: dataControllerDelegate)
+        self.mappedCartProductsDelegate = mappedCartProductsDelegate
+    }
+
+    required init(dataControllerDelegate: DataControllerDelegate) {
+        super.init(dataControllerDelegate: dataControllerDelegate)
+    }
+
+    func fetchProductsForMapping() {
+        NetworkHelper<[Product]>.makeRequest(path: "products", onSuccess: {
+            [weak self] data in
+            if (data.isEmpty) {
+                self?.mappedCartProductsDelegate?.cartProductsFetchingFailed(errorMessage: "No items found")
+                return
+            }
+
+            //self?.data = data
+            //self?.dataControllerDelegate.dataRetrieved(data: data)
+        }){ [weak self] errorMessage in
+            self?.mappedCartProductsDelegate?.cartProductsFetchingFailed(errorMessage: errorMessage)
+
+        }
+    }
+
+    func itemForRow(row: Int) -> Product? {
         return nil
     }
     
