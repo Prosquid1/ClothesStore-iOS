189d1d34b64a5590433a7f27423d67a5373dfef3
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-21 13:19:25 +0100

    Adding server secrets file

---
+++ ClothesStore/Helpers/ServerSecrets.swift
@@ -0,0 +1,15 @@
+//
+//  ServerSecrets.swift
+//  ClothesStore
+//
+//  Created by Oyeleke Okiki on 7/21/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import Foundation
+
+struct ServerSecrets {
+    static let API_KEY="API_KEY_HERE"
+    static let BASE_URL="BASE_URL_HERE"
+    static let HEADER_AUTH_KEY="AUTH_KEY_HERE"
+}
